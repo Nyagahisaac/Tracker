894576bc48fc045974f2df16a32e5674217aa6c2
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-02 06:21:27 +0300

    fix repo pull

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   clientSecret: "9625d32e6a8ec69317e8a157ab8842cca22f64d4" ,
   clientId:"31b7d285fa392dc61722",
   apiUrl:"https://api.github.com/users/",
-  apikey:"?access_token=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",
-  repoUrl: "https:api.github.com/search/repositories/"
+  apikey:"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",
+  repoUrl: "https://api.github.com/search/repositories/"
 };
