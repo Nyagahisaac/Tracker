98c590a1dc71c9d9cb664a6d9043bc1b352cc046
isaac.ireri <isaac.ireri@student.moringaschool.com>
2021-09-02 03:13:16 +0300

    fix deploy changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  clientSecret: "9625d32e6a8ec69317e8a157ab8842cca22f64d4" ,
+  clientId:"31b7d285fa392dc61722",
+  apiUrl:"https://api.github.com/users/",
+  apikey:"?access_token=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",
+  repoUrl: "https:api.github.com/search/repositories/"
 };
